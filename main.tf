90b91ed0d307e6d8ae2735e81dad193a03772803
Amaan Mansuri <165874699+Amaan00101@users.noreply.github.com>
2026-01-26 22:53:08 +0530

    Update echo command in null_resource example

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "example" {
   provisioner "local-exec" {
-    command = "echo Hellooo, Atlantissssssssssss!"
+    command = "echo Hellooo, Atlantissssssssaaaaaassss!"
   }
 }
