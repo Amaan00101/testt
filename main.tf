a91c599346d8e3c5214b884663e419cdec32cd26
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 <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>
2026-01-27 18:33:09 +0530

    second branch commit for lock test

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "null_resource" "example" {
   provisioner "local-exec" {
-    command = "echo Hellooo, Atlantissssiaaa!"
+    command = "echo Hellooo, Atlantissssiaaaaaaaaaaaaaaaaaaaaaaaaa!"
   }
 }
 
