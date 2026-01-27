3203ac84b2f38de5b9a8d602b64deb662f6a124a
Amaan Mansuri <165874699+Amaan00101@users.noreply.github.com>
2026-01-27 15:20:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "null_resource" "example" {
   provisioner "local-exec" {
-    command = "echo Hellooo, Atlantissssssssssss!"
+    command = "echo Hellooo, Atlantissssssss!"
   }
 }
 
